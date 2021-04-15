553c1851e0d672d030009386537888d4a0560b6d
Vitalii Sokol <sokol@lanars.com>
2021-04-15 10:19:37 +0300

    Clean up code

--- factory/index.js
+++ factory/index.js
@@ -35,7 +35,7 @@ class Basketball {
 const factory = new BallFactory();
 const myFootball = factory.createBall('football');
 const myBasketball = factory.createBall('basketball');
-console.log(myFootball.roll()); // The football is rolling.
-console.log(myBasketball.roll()); // The basketball is rolling.
-console.log(myFootball.kick()); // You kicked the football.
-console.log(myBasketball.bounce()); // You bounced the basketball.
+console.log(myFootball.roll());
+console.log(myBasketball.roll());
+console.log(myFootball.kick());
+console.log(myBasketball.bounce());
